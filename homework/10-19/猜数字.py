466f50241b98b3445593fa7bed92484ae0350de7
LHT-Stan <lihongting_email@163.com>
2022-10-24 19:44:07 +0800

    台式上传

--- homework/10-19/猜数字.py
+++ homework/10-19/猜数字.py
@@ -1,6 +1,13 @@
 import random
 Range = random.randint(1, 100)
 number = random.randint(1, Range)
+print(f"---电脑已经随机生成1-{Range}之间的整数")
 while True:
-    print(f"---电脑已经随机生成1-{Range}之间的整数")
-    
+    guess = int(input("请输入您要猜的数字："))
+    if number == guess:
+        print("恭喜你猜对了")
+        break
+    elif number < guess:
+        print("您猜的数大了，重新试一下吧")
+    elif number > guess:
+        print("您猜的数小了，重新试一下吧")
